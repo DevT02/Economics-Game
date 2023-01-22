b5a5e35af8ae482b848e8a1883092dc1da9d91b9
Devansh Tayal <devliveforever@gmail.com>
2023-01-22 00:27:27 -0500

    title screen and character select

---
+++ scenes/titlescreen.gd
@@ -0,0 +1,21 @@
+extends Control
+
+export(ButtonGroup) var group
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for i in group.get_buttons():
+		i.connect("pressed", self, "button_pressed")
+		
+func button_pressed():
+	if group.get_pressed_button() == group.get_buttons()[0]:
+		get_tree().change_scene("res://scenes/characterSelectionScreen.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
