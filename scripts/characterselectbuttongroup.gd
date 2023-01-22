40c02dbc4ee08e99d7a87958f8af67ad39d62d81
Devansh Tayal <devliveforever@gmail.com>
2023-01-22 12:25:41 -0500

    data aggregation thru json for interactions
    interaction unimplemented

--- scripts/characterselectbuttongroup.gd
+++ scripts/characterselectbuttongroup.gd
@@ -1,7 +1,6 @@
 extends Control
 
 export(ButtonGroup) var group
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -11,13 +10,10 @@ export(ButtonGroup) var group
 func _ready():
 	for i in group.get_buttons():
 		i.connect("pressed", self, "button_pressed")
-		
-func button_pressed():
-	if group.get_pressed_button() == group.get_buttons()[0]:
-		get_tree().change_scene("res://scenes/characterselectionscreen.tscn")
-#	elif group.get_pressed_button() == group.get_buttons()[0]:
-#		get_tree().change_scene("res://scenes/characterselectionscreen.tscn")
 
+func button_pressed():
+	if group.get_pressed_button() == group.get_buttons()[1]:
+		get_tree().change_scene("res://scenes/world.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
