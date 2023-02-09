64c555696c5d426596028adfbd4741bb6a7df5e0
Devansh Tayal <devliveforever@gmail.com>
2023-02-09 11:11:58 -0500

    fix animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,20 +9,25 @@ func read_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("up"):
 		animation.play("walk_up")
-		velocity.y -= 1
+		velocity.y -= 0.5
 		direction = Vector2(0, -1)
-	if Input.is_action_pressed("down"):
+	elif Input.is_action_pressed("down"):
 		animation.play("walk_down")
-		velocity.y += 1
+		velocity.y += 0.5
 		direction = Vector2(0, 1)
-	if Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left"):
 		animation.play("walk_left")
-		velocity.x -= 1
+		velocity.x -= 0.5
 		direction = Vector2(-1, 0)
-	if Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right"):
 		animation.play("walk_right")
-		velocity.x += 1
+		velocity.x += 0.5
 		direction = Vector2(1, 0)
+	else:
+		animation.seek(0, true)
+		animation.stop()
+		
+
 	velocity = velocity.normalized()
 	velocity = move_and_slide(velocity * 200)
 	
