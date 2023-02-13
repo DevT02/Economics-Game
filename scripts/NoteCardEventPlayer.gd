0a46337816efde981216c1e447cced9df2c1b562
Devansh Tayal <devliveforever@gmail.com>
2023-02-13 13:33:03 -0500

    setup vars for randomize

--- scripts/NoteCardEventPlayer.gd
+++ scripts/NoteCardEventPlayer.gd
@@ -5,6 +5,13 @@ export(String, FILE, "*.json") var data_file
 var data = []
 var eventId = 0
 var dialogue_active = false
+
+var current_company = "tech"
+var from_department = current_company + "_from_events"
+var current_events = current_company + "_events"
+var current_choices = current_company + "_choices"
+var current_outcomes = current_company + "_outcomes"
+
 #var rng = RandomNumberGenerator.new()
 
 func _ready():
@@ -21,13 +28,8 @@ func play():
 		
 	dialogue_active = true
 	$NinePatchRect.visible = true
-	$NinePatchRect/ToLabel.text = data[eventId]['name']
-	$NinePatchRect/FromLabel.text = data[3]['tech_from_events'][eventId]
-	$NinePatchRect/MessageLabel.text = data[2]['tech_events'][eventId]
-	
-	$NinePatchRect/Option1Button.text = data[4]['tech_choices'][eventId]
-	$NinePatchRect/Option2Button.text = data[4]['tech_choices'][eventId+1]
-	$NinePatchRect/Option3Button.text = data[4]['tech_choices'][eventId+2]
+	eventId=-1
+	_nextEvent()
 	
 #func randomize_event_number():
 #	eventId = rng.randf_range(0, data[3]['tech_choices'] - 1)
@@ -35,17 +37,29 @@ func play():
 func _nextEvent():
 	eventId += 1
 	$NinePatchRect/ToLabel.text = data[eventId]['name']
-	$NinePatchRect/FromLabel.text = data[3]['tech_from_events'][eventId]
-	$NinePatchRect/MessageLabel.text = data[2]['tech_events'][eventId]
+	$NinePatchRect/FromLabel.text = data[3][from_department][eventId]
+	$NinePatchRect/MessageLabel.text = data[2][current_events][eventId]
 	
-	$NinePatchRect/Option1Button.text = data[4]['tech_choices'][eventId]
-	$NinePatchRect/Option2Button.text = data[4]['tech_choices'][eventId+1]
-	$NinePatchRect/Option3Button.text = data[4]['tech_choices'][eventId+2]
+	$NinePatchRect/Option1Button.text = data[4][current_choices][eventId]
+	$NinePatchRect/Option2Button.text = data[4][current_choices][eventId+1]
+	$NinePatchRect/Option3Button.text = data[4][current_choices][eventId+2]
 	
 	if eventId >= len(data):
 		dialogue_active = false
 		$NinePatchRect.visible = false 
 		return	
+		
+func _randomize(company):
+
+	match company:	
+		"tech":
+			print("X")
+		"fast_food":
+			print("Y")
+		"fashion":
+			print("Z")
+
+	
 
 func _input(event):
 	if not dialogue_active:
