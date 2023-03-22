6f3d6ff22216ec8dfb92fca187493514b4b3ebec
Devansh Tayal <devliveforever@gmail.com>
2023-03-22 00:47:24 -0400

    null handling of text (outcomes)

--- scripts/NoteCardEventPlayer.gd
+++ scripts/NoteCardEventPlayer.gd
@@ -25,9 +25,9 @@ var profit = 0
 var public_img = 0
 var stakeholder = 0
 var text_speed = 0.02
-
-
-
+var button1Index = null
+var button2Index = null
+var button3Index = null
 ## PUBLIC IMAGE, STAKHOLDER, PROFIT
 
 #var rng = RandomNumberGenerator.new()
@@ -36,11 +36,6 @@ var text_speed = 0.02
 func _ready():
 	$NinePatchRect.visible = false
 	get_node("../EffectsPopUp/Tween/NinePatchRect/Label").visible = false
-
-
-
-
-	
 	for i in group.get_buttons():
 		i.connect("pressed", self, "button_pressed")
 	
@@ -55,19 +50,18 @@ func load_data():
 func button_pressed():
 	if $NinePatchRect.visible == true:
 		if group.get_pressed_button() == group.get_buttons()[0]:
-			updateEffects(0)		
+			updateEffects(button1Index)		
 			fadeOut()
 		elif group.get_pressed_button() == group.get_buttons()[1]:
-			updateEffects(1)
+			updateEffects(button2Index)
 			fadeOut()
 		elif group.get_pressed_button() == group.get_buttons()[2]:
-			updateEffects(2)
+			updateEffects(button3Index)
 			fadeOut()
 		print(profit)
 		print(public_img)
 		print(stakeholder)
-		
-		
+
 # when pressed (see interactions.gd)
 func play():
 	if dialogue_active:
@@ -76,7 +70,6 @@ func play():
 	fadeIn()
 	randomizeEvents()
 	
-
 func randomizeEvents():
 	match current_company:	
 		"tech":	
@@ -102,21 +95,22 @@ func fadeOut():
 	dialogue_active = false
 	
 func _nextEvent(numberofEvents):
-
+	
 	$NinePatchRect/ToLabel.text = data['name']
 	# fromLabels correspond with messageLabel (in size)
 	$NinePatchRect/FromLabel.text = data[current_company][from_events][numberofEvents]
 	$NinePatchRect/MessageLabel.text = data[current_company][current_events][numberofEvents]
 	
-	print(numberofEvents)
 	var button1 = (numberofEvents) * 3 + (generate_random_number(1, 3, used_numbers3, 3))
 	var button2 = (numberofEvents) * 3 + (generate_random_number(1, 3, used_numbers3, 3))
 	var button3 = (numberofEvents) * 3 + (generate_random_number(1, 3, used_numbers3, 3))
-
+	button1Index = button1 - 1
+	button2Index = button2 - 1
+	button3Index = button3 - 1
 	# each event has corresonding 3 fromEvents, subtract 1 as we are using index, multiply by 3 as 3 per choice. 
-	var data1 = data[current_company][current_choices][button1-1] 
-	var data2 = data[current_company][current_choices][button2-1] 
-	var data3 = data[current_company][current_choices][button3-1] 
+	var data1 = data[current_company][current_choices][button1Index] 
+	var data2 = data[current_company][current_choices][button2Index] 
+	var data3 = data[current_company][current_choices][button3Index] 
 
 	$NinePatchRect/Option1Button.text = data1 if data1 != null else ''
 	$NinePatchRect/Option2Button.text = data2 if data2 != null else ''
@@ -140,13 +134,20 @@ func erase():
 	data[4][current_choices].erase(eventId+1)
 	data[4][current_choices].erase(eventId+2)
 
-func updateEffects(buttonChoice):
+func updateEffects(outcomeIndex):
+	# see in this function, we dont have to worry about null values as they will be set to 0
 	# load text first then logic
-	var text = data[current_company][current_outcomes][used_numbers3[buttonChoice]]
+	var text = data[current_company][current_outcomes][outcomeIndex]
+	print(text)
+	# stop everything if data is null
+	if text != "Null":
+		get_node("../EffectsPopUp/Tween/NinePatchRect/Label").text = text
+	else:
+		updateNumericalEffects(outcomeIndex)
+		return
 	
 	get_node("../EffectsPopUp/Tween/NinePatchRect/Label").visible = true
 	get_node("../EffectsPopUp/Tween/NinePatchRect/Label").visible_characters = 0
-	get_node("../EffectsPopUp/Tween/NinePatchRect/Label").text = text
 	var text_length = len(text)
 	var text_reveal_speed = text_length * text_speed
 	get_node("../EffectsPopUp/Tween/").interpolate_property(get_node("../EffectsPopUp/Tween/NinePatchRect/Label"), "visible_characters", 0, text_length, text_reveal_speed, Tween.TRANS_LINEAR)
@@ -155,11 +156,9 @@ func updateEffects(buttonChoice):
 	var oldProfit = profit
 	var oldImg = public_img
 	var oldStakeholder = stakeholder
-	profit += data[current_company][current_outcome_profit][used_numbers3[buttonChoice]]	
-	public_img += data[current_company][current_outcome_image_effects][used_numbers3[buttonChoice]]	
-	stakeholder += data[current_company][current_outcome_stakeholder][used_numbers3[buttonChoice]]	
+	updateNumericalEffects(outcomeIndex)
 	
-	# based on differences, match a color to the text 0-0.5 is YELLOW (or okay) anything below 0 is RED (bad) anything above 0.5 is GREEN (great)
+	# based on numerical differences, match a color to the text 0-0.5 is YELLOW (or okay) anything below 0 is RED (bad) anything above 0.5 is GREEN (great)
 	var diffProfit = profit - oldProfit
 	var diffImg = public_img - oldImg
 	var diffStakeholder = stakeholder - oldStakeholder
@@ -170,6 +169,10 @@ func updateEffects(buttonChoice):
 		buttonEffect = 2
 	elif (0 < diffStakeholder && diffStakeholder <= 1) || (0 < diffImg && diffImg <= 1) || (0 < diffProfit && diffProfit <= 1):
 		buttonEffect = 0
+	else:
+		# idk?
+		buttonEffect = 4
+		
 	
 	# switch effect label
 	match buttonEffect:
@@ -182,12 +185,19 @@ func updateEffects(buttonChoice):
 		2:
 			# (YELLOW)
 			get_node("../EffectsPopUp/Tween/NinePatchRect/Label").add_color_override("font_color", Color8(241, 255, 0, 255))
+		4:
+			# (cool effect maybe? or if == 0)
+			get_node("../EffectsPopUp/Tween/NinePatchRect/Label").add_color_override("font_color", Color8(0, 0, 255, 255))
 
 
-
-	pressedButton = buttonChoice;
+	pressedButton = outcomeIndex;
 	# reset randomization (so we don't repeat numbers)
 	used_numbers3 = []
+
+func updateNumericalEffects(outcomeIndex):
+	profit += data[current_company][current_outcome_profit][outcomeIndex]	
+	public_img += data[current_company][current_outcome_image_effects][outcomeIndex]	
+	stakeholder += data[current_company][current_outcome_stakeholder][outcomeIndex]	
 
 func generate_random_number(range_start, range_end, known_numbers, usedNumberIndex):
 	# Track the numbers that have been generated so far	
