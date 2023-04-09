d37801065196b593e76778ec2ac41b783dd10ad5
Devansh Tayal <devliveforever@gmail.com>
2023-04-09 15:07:57 -0400

    created basic map

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,26 +14,25 @@ func read_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("up"):
 		animation.play("walk_up")
-		velocity.y -= 0.5
-		direction = Vector2(0, -1)
+		velocity.y -= 3
+		direction = Vector2(0, -0.5)
 	elif Input.is_action_pressed("down"):
 		animation.play("walk_down")
-		velocity.y += 0.5
-		direction = Vector2(0, 1)
+		velocity.y += 3
+		direction = Vector2(0, 0.5)
 	elif Input.is_action_pressed("left"):
 		animation.play("walk_left")
-		velocity.x -= 0.5
-		direction = Vector2(-1, 0)
+		velocity.x -= 3
+		direction = Vector2(-0.5, 0)
 	elif Input.is_action_pressed("right"):
 		animation.play("walk_right")
-		velocity.x += 0.5
-		direction = Vector2(1, 0)
+		velocity.x += 3
+		direction = Vector2(0.5, 0)
 	else:
 		animation.seek(0, true)
 		animation.stop()
 		
 
-	velocity = velocity.normalized()
 	velocity = move_and_slide(velocity * 200)
 	
 
